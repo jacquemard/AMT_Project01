704643538afd7ab55135b97d50f4cd842df779b0
Nadir Benallal <nadir.benallal@heig-vd.ch>
2017-09-29 16:29:48 +0200

    Creating model and main packages

---
+++ Nackeskratta/src/main/java/ch/heigvd/nackeskratta/model/Furniture.java
@@ -0,0 +1,71 @@
+package ch.heigvd.nackeskratta.model;
+
+import java.awt.Color;
+
+public class Furniture
+{
+   private String name;
+   private Category category;
+   private Material material;
+   private Color color;
+   private float price;
+   
+   Furniture(String name, Category cat, Material mat, Color col, float price)
+   {
+      this.name = name;
+      this.category = cat;
+      this.material = mat;
+      this.color = col;
+      this.price = price;
+   }
+
+   public String getName()
+   {
+      return name;
+   }
+
+   public Category getCategory()
+   {
+      return category;
+   }
+
+   public Material getMaterial()
+   {
+      return material;
+   }
+
+   public Color getColor()
+   {
+      return color;
+   }
+
+   public float getPrice()
+   {
+      return price;
+   }
+
+   public void setName(String name)
+   {
+      this.name = name;
+   }
+
+   public void setCategory(Category category)
+   {
+      this.category = category;
+   }
+
+   public void setMaterial(Material material)
+   {
+      this.material = material;
+   }
+
+   public void setColor(Color color)
+   {
+      this.color = color;
+   }
+
+   public void setPrice(float price)
+   {
+      this.price = price;
+   }
+}
