a2fe9c10a505d5ce568d1f6b4ca7a957855fbcd5
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2017-10-04 16:37:05 +0200

    continuation MVC

---
+++ Nackeskratta/src/main/java/ch/heigvd/nackeskratta/controllers/FurnitureServlet.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.nackeskratta.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Remi
+ */
+public class FurnitureServlet extends HttpServlet {
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
+	
+	
+    }
+
+   
+}
---
+++ Nackeskratta/src/main/java/ch/heigvd/nackeskratta/services/FurnitureManager.java
@@ -0,0 +1,16 @@
+package ch.heigvd.nackeskratta.services;
+
+import ch.heigvd.nackeskratta.model.Category;
+import ch.heigvd.nackeskratta.model.Furniture;
+
+/**
+ *
+ * @author Remi
+ */
+public class FurnitureManager {
+    
+    public Furniture getRandomFurniture(){
+	return null;
+    }
+    
+}
